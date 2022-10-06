38797bf4ecaa9be7e1c5f9febb357a0889013a34
KiptolStud <Kiptol@yandex.ru>
2022-10-06 22:36:10 +0300

    first commit

---
+++ Hw3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+int Prompt(string message)
+
+{    
+    Console.Write(message);
+    string firststring = Console.ReadLine();
+    int value1 = Convert.ToInt32 (firststring);
+    return value1;
+}
+int value1 = Prompt("Введите число для проверки четности ");        
+        if ((value1  % 2) == 0)
+        {
+            System.Console.WriteLine($"Число {value1} является четным");
+        }
+        else
+        {
+            System.Console.WriteLine($"Число {value1} является нечетным");
+        }
---
+++ Hw4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int Prompt(string message)
+{    
+    Console.Write(message);
+    string firststring = Console.ReadLine();
+    int value1 = Convert.ToInt32 (firststring);
+    return value1;
+}
+int valuemin = Convert.ToInt32("1");
+int value1 = Prompt("Введите количество элементов массива ");
+        for (int i = valuemin; i <= value1; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                System.Console.WriteLine(i + " ");
+            }
+        }
+   
