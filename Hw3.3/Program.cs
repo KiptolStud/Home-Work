bea6abd2f2d80099ce13b799e891d368b24f55ed
KiptolStud <113547691+KiptolStud@users.noreply.github.com>
2022-10-10 21:28:09 +0300

    Update Program.cs

--- Hw3.3/Program.cs
+++ Hw3.3/Program.cs
@@ -4,18 +4,18 @@ string writeN = Console.ReadLine();
 int N = Convert.ToInt32(writeN);
 int[] GetCubeTable(int N)
 {
-    int[] cubs = new int[N];
-    int number = 1;
-    for (int i = 0; i < N; i++)
+int[] cubs = new int[N];
+int number = 1;
+for (int i = 0; i < N; i++)
     {
         cubs[i] = number * number * number;
         number++;
     }
-    return cubs;
+return cubs;
 }
 void PrintCubs(int[] array)
 {
-    int number = 1;
+int number = 1;
     for (int i = 0; i < array.Length; i++)
     {
         System.Console.Write($"{number} ^ 3 = ");
