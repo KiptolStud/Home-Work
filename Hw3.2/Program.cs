78dbe77562556489668b0949ccc05d23001c8982
KiptolStud <Kiptol@yandex.ru>
2022-10-10 21:24:25 +0300

    add HomeWork 3 seminar

---
+++ Hw3.2/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{ public static void Main()
+{ double [] x = new double [6];
+double d, distance = 0.0;
+Console.WriteLine("Введите кординаты двух точек через пробел");
+Console.WriteLine("x1 y1 z1 x2 y2 z2:");
+var line = Console.ReadLine().Split();
+for (int i = 0; i < 3; i++)
+{ d = Convert.ToDouble(line[i + 3])
+- Convert.ToDouble(line[i]);
+distance += d * d; }
+Console.WriteLine("Расстояние между двумя точками = " + Math.Sqrt(distance)); } } 
