092abeeb4f61e89fc5400bcb8a6906d90b3b8eb7
KiptolStud <Kiptol@yandex.ru>
2022-10-10 21:25:57 +0300

    add all HomeWork 3 seminar

---
+++ Hw3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+System.Console.Write("Введите пятизначное число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  /// для проверки текста на палиндром
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+if (IsPalindrome()) System.Console.WriteLine("Да");
+else System.Console.WriteLine("Нет");
+System.Console.WriteLine();
