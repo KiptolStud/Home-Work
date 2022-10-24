0e51d1c224bb5968f4263e28e2508935d8fda438
KiptolStud <Kiptol@yandex.ru>
2022-10-24 13:40:52 +0300

    add homework seminar 7

---
+++ Hw7.1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.Clear();
+System.Console.WriteLine();
+
+int[,] GetTable(int row, int col, int min, int max)
+{
+    int[,] table = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            table[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return table;
+}
+
+void PrintTable(int[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            System.Console.Write(tab[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = new Random().Next(3, 4);
+int col = new Random().Next(3, 4);
+int min = -10;
+int max = 10;
+
+int[,] matrix = GetTable(row, col, min, max);
+PrintTable(matrix);
---
+++ Hw7.3/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.Clear();
+
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] >= 10) System.Console.Write($" {array[i, j]}  ");
+            else System.Console.Write($"  {array[i, j]}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[] GetArithmeticMean(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+            result[j] = sum / array.GetLength(1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[] array)
+{
+    System.Console.WriteLine("------------------------");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}  ");
+    }
+}
+
+int row = 3;
+int col = 4;
+int min = 1;
+int max = 10;
+
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+double[] arithmeticMean = GetArithmeticMean(matrix);
+PrintArray(arithmeticMean);
+System.Console.WriteLine();
+System.Console.WriteLine();
